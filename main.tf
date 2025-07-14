18a978ff9cb4cc1ce06cdd5b1d99a1039f69a916
Ygarcia8 <Yasming1608@gmail.com>
2025-07-13 23:01:33 -0400

    Update main.tf
    “Edit the main.tf on ‘staging’ branch, create a security group only and make it specific.”

--- main.tf
+++ main.tf
@@ -1,42 +1,19 @@
-name: tfsec
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = "vpc-12345678"
 
-on:
-  pull_request:
-    branches:
-      - staging
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["203.0.113.0/24"] # Específico, no 0.0.0.0/0
+  }
 
-jobs:
-  build:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v3
-
-      - name: Set up Terraform
-        uses: hashicorp/setup-terraform@v2
-        with:
-          terraform_version: 1.5.7
-
-      - name: Check for Terraform files
-        run: |
-          if ls *.tf 1> /dev/null 2>&1; then
-            echo "Terraform files found"
-          else
-            echo "No Terraform files found"
-            exit 1
-          fi
-
-      - name: Install tfsec
-        run: |
-          curl -s https://raw.githubusercontent.com/aquasecurity/tfsec/master/scripts/install_linux.sh | bash
-
-      - name: Run tfsec
-        run: tfsec --soft-fail .
-
-      - name: Install tflint
-        run: |
-          curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
-
-      - name: Run tflint
-        run: tflint --chdir .
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
