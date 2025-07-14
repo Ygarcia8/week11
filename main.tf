8ccb1549a8004f49ea0eaba9dc793625eb6e6e4f
Ygarcia8 <Yasming1608@gmail.com>
2025-07-13 23:07:09 -0400

    Update final main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
-  description = "Allow HTTP traffic"
+  description = "Allow HTTP traffic only from internal network"
   vpc_id      = "vpc-12345678"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["203.0.113.0/24"] # Específico, no 0.0.0.0/0
+    cidr_blocks = ["10.0.0.0/16"] # Ejemplo de rango interno
+    description = "Allow HTTP from internal network"
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"] # Limitar egress al mismo rango interno
+    description = "Allow all traffic within internal network"
   }
 }
